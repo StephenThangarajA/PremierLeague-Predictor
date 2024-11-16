792c7c5cada7f4614f09c07bfd4677ac4a962e52
StephenThangarajA <stephenthangaraj14@gmail.com>
2024-11-16 21:43:51 +0530

    16-11-24

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Match from "./Match";
 
 const App = () => {
     return (
-      <Router>
+      <Router basename="/predictor/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
